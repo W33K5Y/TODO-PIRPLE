5c8e21140c91b0062c8601455c4bcd0ae571997f
Sam Weeks <weeksy_17@icloud.com>
2021-01-29 04:41:10 +0000

    finished the three section DIVS

---
+++ app.js
@@ -0,0 +1,19 @@
+// ! VARIABLES 
+// * sections
+const start = document.getElementById("start");
+const signUp = document.getElementById("sign-up");
+const login = document.getElementById("login");
+
+// * clickables 
+// ? start
+const h1Button = document.querySelector(".h1-b");
+const loginStartB = document.getElementById("login-start-b")
+const signUpStartB = document.getElementById("sign-up-start-b");
+// ? sign-up
+const loginSignUpB = document.getElementById("login-sign-up-b")
+const signUpSignUpB = document.getElementById("sign-up-sign-up-b");
+// ? login
+const loginLoginB = document.getElementById("login-login-b")
+const signUpLoginB= document.getElementById("sign-up-login-b");
+
+h1Button.addEventListener("click", () => console.log('testy'))
