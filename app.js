48edc22933e5c510f9ee6d676af44f269c09d00f
Sam Weeks <weeksy_17@icloud.com>
2021-01-29 21:20:01 +0000

    three sections complete

--- app.js
+++ app.js
@@ -1,19 +1,49 @@
 // ! VARIABLES 
-// * sections
+// ! sections
 const start = document.getElementById("start");
 const signUp = document.getElementById("sign-up");
 const login = document.getElementById("login");
 
-// * clickables 
-// ? start
-const h1Button = document.querySelector(".h1-b");
+// ! clickables 
+// * start
+const h1Button = document.querySelectorAll(".h1-b");
 const loginStartB = document.getElementById("login-start-b")
 const signUpStartB = document.getElementById("sign-up-start-b");
-// ? sign-up
+// * sign-up
 const loginSignUpB = document.getElementById("login-sign-up-b")
 const signUpSignUpB = document.getElementById("sign-up-sign-up-b");
-// ? login
+// * login
 const loginLoginB = document.getElementById("login-login-b")
 const signUpLoginB= document.getElementById("sign-up-login-b");
 
-h1Button.addEventListener("click", () => console.log('testy'))
+// ! SETTING LISTENERS on all clickable nav el's 
+// ? this bellow is iterating through each case of the three identicle section h1's 
+for (const h1 of h1Button) {
+    h1.addEventListener("click",signupLoginNone)
+};
+// *logins
+loginStartB.addEventListener("click", signupStartNoneLoginFlex);
+loginSignUpB.addEventListener("click", signupStartNoneLoginFlex);
+loginLoginB.addEventListener("click", signupStartNoneLoginFlex);
+// *sign ups
+signUpStartB.addEventListener("click", loginStartNoneSignupFlex);
+signUpSignUpB.addEventListener("click", loginStartNoneSignupFlex);
+signUpLoginB.addEventListener("click", loginStartNoneSignupFlex);
+
+// ! FUNCTIONS FOR LISTNERS 
+// ? creating functions to decide what section gets shown 
+function signupLoginNone(e){
+    start.style.display = "flex";
+    signUp.style.display = "none";
+    login.style.display = "none";
+};
+function signupStartNoneLoginFlex(e) {
+    login.style.display = "flex";
+    start.style.display = "none";
+    signUp.style.display = "none";
+}
+function loginStartNoneSignupFlex(e) {
+    signUp.style.display = "flex";
+    start.style.display = "none";
+    login.style.display = "none";
+}
